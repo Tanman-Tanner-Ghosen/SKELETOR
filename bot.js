2b176c69cbe3a68b758a0514add4e6c927cc8722
TANMAN TANNER" GHOSEN <tannerghosen1999@yahoo.com>
2020-12-26 15:20:23 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -292,14 +292,14 @@ client.on('message', async message => {
 		{
 			var embedhelpmember = new Discord.RichEmbed()
 			.setTitle("**SKELETOR**\n")
-			.addField("Bot Help",`I am the SKELETOR Discord Bot, Version 1.666, 2019-2022 by "TANMAN TANNER" Ghosen.\nPrefix for the following commands is '`+prefix+"'.\n Commands: dice, rps, 8ball, funny, kick, ban, purge, ping, bothelp");
+			.addField("Bot Help",`I am the SKELETOR Discord Bot, Version 1.6669, 2019-2022 by "TANMAN TANNER" Ghosen.\nPrefix for the following commands is '`+prefix+"'.\n Commands: dice, rps, 8ball, funny, kick, ban, purge, ping, bothelp");
 			message.channel.send(embedhelpmember);
 		}
 		else
 		{
 			var embedhelpmember = new Discord.RichEmbed()
 			.setTitle("**SKELETOR**\n")
-			.addField("Bot Help",`I am the SKELETOR Discord Bot, Version 1.666, 2019-2022 by "TANMAN TANNER" Ghosen.\nPrefix for the following commands is '`+prefix+"'.\n Commands: dice, rps, 8ball, kick, ban, purge, ping, bothelp");
+			.addField("Bot Help",`I am the SKELETOR Discord Bot, Version 1.6669, 2019-2022 by "TANMAN TANNER" Ghosen.\nPrefix for the following commands is '`+prefix+"'.\n Commands: dice, rps, 8ball, kick, ban, purge, ping, bothelp");
 			message.channel.send(embedhelpmember);
 		}
 	}
